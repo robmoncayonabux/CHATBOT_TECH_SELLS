7e486cd32f7c15dc83f58f0b58555d763346c899
robmoncayonabux <roberto.moncayo@nabux.ec>
2023-09-18 15:05:49 -0500

    Flow folder

---
+++ flujos/eventsFlows.js
@@ -0,0 +1,18 @@
+const { addKeyword, EVENTS } = require('@bot-whatsapp/bot')
+
+const REGEX_CREDIT_NUMBER = require('./events/creditCard')
+const REGEX_EMAIL = require('./events/email')
+
+
+
+const flowDocumento = addKeyword(EVENTS.DOCUMENT).addAnswer('Para que este documento?')
+
+const flowMedia = addKeyword(EVENTS.MEDIA).addAnswer('Y que quieres que haga con este archivo media?')
+
+const flowVoiceNote = addKeyword(EVENTS.VOICE_NOTE).addAnswer('Interesante... me has dado un voicenote')
+
+const flowTarjetaDeCredito =addKeyword(REGEX_CREDIT_NUMBER.toString(), { regex: true }).addAnswer('Guau una tarjeta de credito')
+
+const flowEmail =addKeyword(REGEX_EMAIL.toString(), { regex: true }).addAnswer('Si, si ya te envio un correo..')
+
+module.export = [flowDocumento, flowMedia, flowEmail, flowTarjetaDeCredito, flowVoiceNote]
---
+++ flujos/flows.js
@@ -0,0 +1,29 @@
+const { addKeyword } = require('@bot-whatsapp/bot')
+
+const axios = require('axios').default
+
+
+
+const API = "https://fakestoreapi.com/products";
+
+const flowPrecio = addKeyword('Precio').addAnswer(
+  "Consultando en la base de datos...",
+  null,
+  async (ctx, { flowDynamic }) => { 
+    let contador = 1
+    const respuesta = await axios(API);
+
+    for (const item of respuesta.data) {
+      console.log(item)
+      if (contador > 3) break;
+      contador++;
+      flowDynamic({ body: [item.title, ` precio: ${item.price}`], media: item.image })
+;
+    }
+  }
+);
+
+
+
+
+module.exports = flowPrecio
