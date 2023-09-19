54c24a47d2fb9df385e4723f4f245b7bb4004f6b
robmoncayonabux <roberto.moncayo@nabux.ec>
2023-09-18 19:21:58 -0500

    We continuo with the testing

---
+++ README.txt
@@ -0,0 +1,2 @@
+Actualizar el chatBot:
+npm install @bot-whatsapp/bot@latest @bot-whatsapp/cli@latest @bot-whatsapp/database@latest @bot-whatsapp/portal@latest
--- app.js
+++ app.js
@@ -4,13 +4,42 @@ const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
-const flowPrecio = require('./flujos/flows')
+const flowPrecio = require('./flows/flows')
+const ticketsSave = require('./ticket/ticket')
 
-const flowSaludo = addKeyword("Hola").addAnswer("Escribe Precio", null, null, [flowPrecio]);
+let GLOBLAL_STATE = {};
+
+const flowPrincipal = addKeyword("Hola")
+  .addAnswer("Hola bienvenido hermano")
+  .addAnswer(
+    "Ya vamos a comenzar con tu pedido... necesitamos la siguiente info"
+  )
+  .addAnswer("Cual es tu nombre?", { capture: true }, async (ctx) => {
+    GLOBLAL_STATE[ctx.from] = {
+      name: ctx.body,
+      description: "",
+    };
+  })
+  .addAnswer("Cual es tu direccion?", { capture: true }, async (ctx) => {
+    console.log("Este numero escribio", ctx.from);
+    GLOBLAL_STATE[ctx.from].direction = ctx.body;
+  })
+  .addAnswer(
+    "Tienes algun codigo?",
+    { capture: true },
+    async (ctx) => {
+      GLOBLAL_STATE[ctx.from].promotion = ctx.body;
+      console.log("Esta info es la que se recolecto", GLOBLAL_STATE[ctx.from]);
+    }
+  )
+  .addAnswer("Procesando tu pedido...", null, async (ctx, { flowDynamic }) => {
+    const ticketAnswer = await ticketsSave(GLOBLAL_STATE[ctx.from]);
+    await flowDynamic(`TU ORDEN/TICKET ES EL # ${ticketAnswer.data.data.id}`);
+  });
 
 const main = async () => {
   const adapterDB = new MockAdapter();
-  const adapterFlow = createFlow([flowSaludo]);
+  const adapterFlow = createFlow([flowPrincipal]);
   const adapterProvider = createProvider(BaileysProvider);
 
   createBot({
