21226dc7b5e4fa0323b3800c6147fe31cb6c11a5
robmoncayonabux <roberto.moncayo@nabux.ec>
2023-09-21 15:55:36 -0500

    feature: src | feat: welcomeFlow

--- app.js
+++ app.js
@@ -1,64 +1,16 @@
 
-const { addKeyword, createBot, createProvider, createFlow} = require('@bot-whatsapp/bot')
-const ChatGPTClass = require('./chatgpt.class.js')
+const { createBot, createProvider, createFlow} = require('@bot-whatsapp/bot')
 
-const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const QRPortalWeb = require('@bot-whatsapp/portal')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
-const PROMPT = require('./promp.js')
-
-const ChatGPTInstance = new ChatGPTClass()
-
-let swith = false;
-
-const flowConfirmacion = addKeyword('SI CONFIRMO').addAnswer('Su cita a sido programada!')
-
-const flowReserva = addKeyword('reserva')
-.addAction(async(ctx, {flowDynamic, ednFlow}) =>{
-  if(!swith){
-    await flowDynamic('Estamos cerrados vuelva mañana')
-    await ednFlow()
-  }
-})
-.addAnswer('Continuamos con tu reserva...')
-
-
-const flowOn = addKeyword('prender').addAction(() => swith = true).addAnswer('Me encendi');
-const flowOff = addKeyword('apagar').addAction(()=> swith = false).addAnswer('Me apague')
-
-const flowInicial = addKeyword("Hola")
-  .addAnswer(
-    [
-      "Buenas! Bienvenido al Chat del Doctor Don Pepe que se maneja con IA 👨‍💻!",
-      "Soy Veronica, en que puedo ayudarte?",
-    ],
-    null,
-    async () => {
-      try {
-        await ChatGPTInstance.handleMsgChatGPT(PROMPT);
-      } catch (error) {
-        console.log(error)
-      }
-    }
-  )
-  .addAnswer(
-    "Solicita una cita con el Doctor Don Pepe! 👨‍⚕️",
-    { capture: true },
-    async (ctx, { flowDynamic, fallBack }) => {
-      const response = await ChatGPTInstance.handleMsgChatGPT(ctx.body);
-      const message = response.text;
-      if (ctx.body.toString() !== "SI CONFIRMO") {
-        await fallBack(message);
-      }
-    },
-  [flowConfirmacion]
-  );
+const welcomeFlow = require('./src/flow/welcomeFlow')
 
 
   const main = async () => {
     const adapterDB = new MockAdapter();
-    const adapterFlow = createFlow([flowInicial, flowOff, flowOn, flowReserva])
+    const adapterFlow = createFlow([welcomeFlow])
     const adapterProvider = createProvider(BaileysProvider);
     
     
---
+++ src/utils/regexCreditcard.js
@@ -0,0 +1,3 @@
+const REGEX_CREDIT_NUMBER = /^(?:\d[ -]*?){13,19}$/gm
+
+module.exports = REGEX_CREDIT_NUMBER
