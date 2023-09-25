566f801138c2f91100387a46a9a32cc0b77425c8
robmoncayonabux <roberto.moncayo@nabux.ec>
2023-09-25 04:17:58 -0500

    Estoy cansado jefe....

---
+++ src/utils/codeClientGenerator.js
@@ -0,0 +1,11 @@
+module.exports.generateCustomerCode = () => {
+  // Genera un número aleatorio entre 0 y 999999999999 (12 dígitos)
+  const randomNum = Math.floor(Math.random() * 1_000_000_000_000);
+  
+  // Convierte el número a una cadena y rellena con ceros a la izquierda si es necesario
+  const customerCode = String(randomNum).padStart(12, '0');
+  
+  return customerCode;
+}
+
+
